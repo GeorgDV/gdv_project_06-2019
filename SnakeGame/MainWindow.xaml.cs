ef813abfe6a677c471d0ca78c86760ccee3957b4
eriksalm90 <eriksalm90@gmail.com>
2019-06-03 14:03:37 +0300

    WPF Start

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace SnakeGame
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double currentLeft = Canvas.GetLeft(rectangle1);
+                Canvas.SetLeft(rectangle1, currentLeft + 20);
+            }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Canvas.SetLeft(rectangle1, 20);
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            double currentTop = Canvas.GetTop(rectangle1);
+            double currentLeft = Canvas.GetLeft(rectangle1);
+            if (e.Key == Key.W)
+            {
+                Canvas.SetTop(rectangle1, currentTop - 20);
+            }
+            else if (e.Key == Key.S)
+            {
+                Canvas.SetTop(rectangle1, currentTop + 20);
+            }
+            else if (e.Key == Key.A)
+            {
+                Canvas.SetLeft(rectangle1, currentLeft - 20);
+            }
+            else if (e.Key == Key.D)
+            {
+                Canvas.SetLeft(rectangle1, currentLeft + 20);
+            }
+        }
     }
 }
