705a1ffe2b90514432f7fcc1c1e801aeb0c5331c
eriksalm90 <eriksalm90@gmail.com>
2019-06-04 14:04:36 +0300

    Changes in Gameboard generating. Added apple generation. Scoreboard.

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -22,18 +22,115 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        string moveDir = "Right";
+
         public MainWindow()
         {
             InitializeComponent();
-            //GAMEGRID
             double XLines = 22;
             double YLines = 22;
             double XSpace = GameBoard.Width / XLines;
             double YSpace = GameBoard.Height / YLines;
+            DrawBoard(XLines, YLines, XSpace, YSpace);
+            InitSnake();
+            GenerateApple(XLines, YLines, XSpace);
+            DispatcherTimer dTimer = new DispatcherTimer();
+            dTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            dTimer.Start();
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            double currentTop = Canvas.GetTop(snake);
+            double currentLeft = Canvas.GetLeft(snake);
+            if (e.Key == Key.W || e.Key == Key.Up)
+            {
+                moveDir = "Up";
+            }
+            else if (e.Key == Key.S || e.Key == Key.Down)
+            {
+                moveDir = "Down";
+            }
+            else if (e.Key == Key.A || e.Key == Key.Left)
+            {
+                moveDir = "Left";
+            }
+            else if (e.Key == Key.D || e.Key == Key.Right)
+            {
+                moveDir = "Right";
+            }
+        }
+
+        public void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            double currentTop = Canvas.GetTop(snake);
+            double currentLeft = Canvas.GetLeft(snake);
+            if (moveDir == "Up")
+            {
+                Canvas.SetTop(snake, currentTop - 30);
+            }
+            else if (moveDir == "Down")
+            {
+                Canvas.SetTop(snake, currentTop + 30);
+            }
+            else if (moveDir == "Right")
+            {
+                Canvas.SetLeft(snake, currentLeft + 30);
+            }
+            else if (moveDir == "Left")
+            {
+                Canvas.SetLeft(snake, currentLeft - 30);
+            }
+        }
+
+        private void DrawBoard(double XLines, double YLines, double XSpace, double YSpace)
+        {
+            //GAMEGRID
+            double startTop;
+            double startLeft;
+
             double X1 = 0;
             double Y1 = 0;
             double X2 = 0;
             double Y2 = GameBoard.Height;
+
+            startTop = -60;
+            for (int i = 0; i < XLines / 2; i++)
+            {
+                startTop += XSpace * 2;
+                startLeft = 0;
+                for (int j = 0; j < YLines / 2; j++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Height = YSpace;
+                    r.Width = XSpace;
+                    r.Fill = Brushes.LightGreen;
+                    Canvas.SetTop(r, startTop);
+                    Canvas.SetLeft(r, startLeft);
+                    startLeft += XSpace * 2;
+                    GameBoard.Children.Add(r);
+                }
+            }
+
+            startTop = -30;
+            for (int i = 0; i < YLines / 2; i++)
+            {
+                startTop += XSpace * 2;
+                startLeft = 30;
+                for (int j = 0; j < YLines / 2; j++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Height = YSpace;
+                    r.Width = XSpace;
+                    r.Fill = Brushes.LightGreen;
+                    Canvas.SetTop(r, startTop);
+                    Canvas.SetLeft(r, startLeft);
+                    startLeft += XSpace * 2;
+                    GameBoard.Children.Add(r);
+                }
+            }
 
             for (int i = 0; i < XLines + 1; i++)
             {
@@ -66,54 +163,31 @@ namespace SnakeGame
                 Y1 += YSpace;
                 Y2 += YSpace;
             }
-            //GAMEGRID/
-
-            //DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            //dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            //dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 250);
-            //dispatcherTimer.Start();
         }
 
-        //private void dispatcherTimer_Tick(object sender, EventArgs e)
-        //{
-        //    double currentTop = Canvas.GetTop(rectangle1);
-        //    double currentLeft = Canvas.GetLeft(rectangle1);
-        //    Canvas.SetLeft(rectangle1, currentLeft + 25);
-        //}
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void InitSnake()
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                double currentLeft = Canvas.GetLeft(rectangle1);
-                Canvas.SetLeft(rectangle1, currentLeft + 30);
-            }
-            if (e.RightButton == MouseButtonState.Pressed)
-            {
-                Canvas.SetLeft(rectangle1, 30);
-            }
+            snake.Height = 30;
+            snake.Width = 30;
+            Canvas.SetTop(snake, GameBoard.Height / 2);
+            Canvas.SetLeft(snake, GameBoard.Width / 2);
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void GenerateApple(double XLines, double YLines, double XSpace)
         {
-            double currentTop = Canvas.GetTop(rectangle1);
-            double currentLeft = Canvas.GetLeft(rectangle1);
-            if (e.Key == Key.W || e.Key == Key.Up)
+            Random rnd = new Random();
+            int appleY = rnd.Next((int)XLines);
+            int appleX = rnd.Next((int)YLines);
+            double snakeCurrTop = Canvas.GetTop(snake);
+            double snakeCurrLeft = Canvas.GetLeft(snake);
+            do
             {
-                Canvas.SetTop(rectangle1, currentTop - 30);
+                Canvas.SetTop(apple, XSpace * appleY + 5);
+                Canvas.SetLeft(apple, XSpace * appleX + 5);
             }
-            else if (e.Key == Key.S || e.Key == Key.Down)
-            {
-                Canvas.SetTop(rectangle1, currentTop + 30);
-            }
-            else if (e.Key == Key.A || e.Key == Key.Left)
-            {
-                Canvas.SetLeft(rectangle1, currentLeft - 30);
-            }
-            else if (e.Key == Key.D || e.Key == Key.Right)
-            {
-                Canvas.SetLeft(rectangle1, currentLeft + 30);
-            }
+            while ((XSpace * appleY) == snakeCurrTop && (XSpace * appleX) == snakeCurrLeft);
         }
+
+
     }
 }
