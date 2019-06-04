fbe1dcc80820c5b10aff31614aafd1b2c7366562
eriksalm90 <eriksalm90@gmail.com>
2019-06-04 11:08:52 +0300

    Added Gameboard, aligned Gameboard grid, scoreboard for future.

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Timers;
 
 namespace SnakeGame
 {
@@ -23,19 +25,72 @@ namespace SnakeGame
         public MainWindow()
         {
             InitializeComponent();
+            //GAMEGRID
+            double XLines = 22;
+            double YLines = 22;
+            double XSpace = GameBoard.Width / XLines;
+            double YSpace = GameBoard.Height / YLines;
+            double X1 = 0;
+            double Y1 = 0;
+            double X2 = 0;
+            double Y2 = GameBoard.Height;
+
+            for (int i = 0; i < XLines + 1; i++)
+            {
+                Line line = new Line();
+                line.Visibility = System.Windows.Visibility.Visible;
+                line.StrokeThickness = 1;
+                line.Stroke = System.Windows.Media.Brushes.ForestGreen;
+                line.X1 = X1;
+                line.Y1 = Y1;
+                line.X2 = X2;
+                line.Y2 = Y2;
+                GameBoard.Children.Add(line);
+                X1 += XSpace;
+                X2 += XSpace;
+            }
+            X1 = 0;
+            X2 = GameBoard.Width;
+            Y2 = 0;
+            for (int i = 0; i < YLines + 1; i++)
+            {
+                Line line = new Line();
+                line.Visibility = System.Windows.Visibility.Visible;
+                line.StrokeThickness = 1;
+                line.Stroke = System.Windows.Media.Brushes.ForestGreen;
+                line.X1 = X1;
+                line.Y1 = Y1;
+                line.X2 = X2;
+                line.Y2 = Y2;
+                GameBoard.Children.Add(line);
+                Y1 += YSpace;
+                Y2 += YSpace;
+            }
+            //GAMEGRID/
+
+            //DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            //dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            //dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 250);
+            //dispatcherTimer.Start();
         }
+
+        //private void dispatcherTimer_Tick(object sender, EventArgs e)
+        //{
+        //    double currentTop = Canvas.GetTop(rectangle1);
+        //    double currentLeft = Canvas.GetLeft(rectangle1);
+        //    Canvas.SetLeft(rectangle1, currentLeft + 25);
+        //}
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 double currentLeft = Canvas.GetLeft(rectangle1);
-                Canvas.SetLeft(rectangle1, currentLeft + 20);
+                Canvas.SetLeft(rectangle1, currentLeft + 30);
             }
-
             if (e.RightButton == MouseButtonState.Pressed)
             {
-                Canvas.SetLeft(rectangle1, 20);
+                Canvas.SetLeft(rectangle1, 30);
             }
         }
 
@@ -43,21 +98,21 @@ namespace SnakeGame
         {
             double currentTop = Canvas.GetTop(rectangle1);
             double currentLeft = Canvas.GetLeft(rectangle1);
-            if (e.Key == Key.W)
+            if (e.Key == Key.W || e.Key == Key.Up)
             {
-                Canvas.SetTop(rectangle1, currentTop - 20);
+                Canvas.SetTop(rectangle1, currentTop - 30);
             }
-            else if (e.Key == Key.S)
+            else if (e.Key == Key.S || e.Key == Key.Down)
             {
-                Canvas.SetTop(rectangle1, currentTop + 20);
+                Canvas.SetTop(rectangle1, currentTop + 30);
             }
-            else if (e.Key == Key.A)
+            else if (e.Key == Key.A || e.Key == Key.Left)
             {
-                Canvas.SetLeft(rectangle1, currentLeft - 20);
+                Canvas.SetLeft(rectangle1, currentLeft - 30);
             }
-            else if (e.Key == Key.D)
+            else if (e.Key == Key.D || e.Key == Key.Right)
             {
-                Canvas.SetLeft(rectangle1, currentLeft + 20);
+                Canvas.SetLeft(rectangle1, currentLeft + 30);
             }
         }
     }
